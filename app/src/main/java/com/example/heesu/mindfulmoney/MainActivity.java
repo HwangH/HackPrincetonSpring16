f90bd7e156c1d9c0bfb96e2f20f80d533e469657
Heesu Hwang <hh5@princeton.edu>
2016-04-02 10:47:50 -0400

    newest Code

--- app/src/main/java/com/example/heesu/mindfulmoney/MainActivity.java
+++ app/src/main/java/com/example/heesu/mindfulmoney/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.heesu.mindfulmoney;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
@@ -17,11 +19,14 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String key = "key=26919f14786c1d9c548f0e653e67c78d";
+    private static String key = "key=f42694af69bea32185ab6de531cff8ab";
+    private static String customer = "56c66be7a73e492741508223";
+    private static String exampleVendor = "56c66be7a73e492741508223";
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
 
@@ -31,17 +36,33 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 5; i++) {
+            try {ServerCall call = new ServerCall(customer, exampleVendor, 5);
+                call.execute();
+            }
+            catch (Exception E) {
+                System.err.println("still network error?");
+                System.err.println(E);
+            }
+        }
+
 
         ArrayList<Purchases> allPurchases = new ArrayList<Purchases>();
         String purchases;
         try {
             purchases = Purchase.getPurchase_Account(key);
             PurchasesList make = new PurchasesList(purchases);
-             allPurchases = make.getList();
+            allPurchases = make.getList();
         } catch (Exception ex) {
-            System.err.println(ex);
+              System.err.println(ex);
         }
 
         average = "" + 5;
@@ -97,5 +118,50 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    private class ServerCall extends AsyncTask<Void, Void, Boolean> {
+
+        private final String a;
+        private final String b;
+        private final int c;
+
+        ServerCall(String a, String b, int c) {
+            this.a = a; this.b = b; this.c =c;
+        }
+
+
+        protected Boolean doInBackground(Void... params) {
+            //this is where you should write your authentication code
+            // or call external service
+            // following try-catch just simulates network access
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            try {
+                System.err.println(Purchase.putPurchase(a, b, c));
+            } catch (Exception E) {
+                System.err.println(E);
+                return false;
+            }
+            return true;
+        }
+
+
+        protected void onPostExecute(final Boolean success) {
+            return;
+        }
+
+        protected void onCancelled() {
+            return;
+        }
+    }
+
+
+
+
 
 }
--- app/src/main/java/com/example/heesu/mindfulmoney/Purchase.java
+++ app/src/main/java/com/example/heesu/mindfulmoney/Purchase.java
@@ -1,6 +1,11 @@
 package com.example.heesu.mindfulmoney;
+import org.json.JSONObject;
+
 import java.net.*;
 import java.io.*;
+import java.text.DateFormat;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 // Must throw exception for all Net functions because of exceptions caused by URL()
 // Example for http request: http://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
@@ -11,7 +16,7 @@ import java.io.*;
 public class Purchase {
 
     // Key used for querying Capitol One Nessie data base
-    private static String key = "key=26919f14786c1d9c548f0e653e67c78d";
+    private static String key = "key=f42694af69bea32185ab6de531cff8ab";
 
     // Parameters: String for Account's ID
     // Returns: URL to request all purchases made from the Account
@@ -47,16 +52,70 @@ public class Purchase {
         return url;
     }
 
+    private static URL makeURL_MakeAPurchase(String account_id, int amount) throws Exception {
+        String prefix = "http://api.reimaginebanking.com/accounts/";
+        String payer = account_id + "/purchases?";
+        String spec = prefix + payer + key;
+        URL url = new URL(spec);
+        return url;
+    }
+
     // Parameters: URL for request
-    // Returns: String represenation of list of JSONs
+    // Returns: String representation of list of JSONs
     private static String sendPurchaseRequest(URL url) throws Exception {
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestProperty("Accept", "application/json");
         String responseMessage = conn.getResponseMessage();
         System.out.println(responseMessage);
         if (!responseMessage.equals("OK")) {
+            System.err.println("ERROR");
             return "ERROR";
         }
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String bodyLine;
+        StringBuffer response = new StringBuffer();
+        while ((bodyLine = in.readLine()) != null) {
+            response.append(bodyLine);
+        }
+        in.close();
+        return response.toString();
+    }
+
+    public static String putPurchase(String account_id, String merchant_id, int amount) throws Exception {
+        URL url = makeURL_MakeAPurchase(account_id, amount);
+        System.err.println(url.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestMethod("POST");
+
+        DateFormat df = new SimpleDateFormat("yyyy/mm/dd");
+
+        JSONObject post = new JSONObject();
+        post.put("merchant_id", merchant_id);
+        post.put("medium", "balance");
+        post.put("purchase_date", df.format(new Date()));
+        post.put("amount", amount);
+        post.put("status", "pending");
+        post.put("description", "string");
+
+        System.err.println("checkpoint");
+
+        OutputStreamWriter wr= new OutputStreamWriter(conn.getOutputStream());
+        wr.write(post.toString());
+
+        wr.close();
+
+
+        String responseMessage = conn.getResponseMessage();
+
+        System.err.println(responseMessage);
+        if (!responseMessage.equals("OK")) {
+            return "ERROR";
+        }
+        System.err.println("checkpoint2");
         BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         String bodyLine;
         StringBuffer response = new StringBuffer();
@@ -71,7 +130,7 @@ public class Purchase {
     // Returns: String representation of list of JSONs for all purchases made from the Account
     public static String getPurchase_Account(String account_id) throws Exception {
         URL url = makeURL_Purchase_Account(account_id);
-        return sendPurchaseRequest(url);
+          return sendPurchaseRequest(url);
     }
 
     // Parameters: String for Merchant's ID, String for Account's ID
